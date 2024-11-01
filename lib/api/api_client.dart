3351ccabc70b7e0eb8ceb5bd9d71c69eb3ed1ac7
thicmn <thichu08@gmail.com>
2024-11-01 11:08:24 +0700

    custome ApiClient

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -1,3 +1,113 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+
 class ApiClient {
-  
+  late final Dio dio;
+
+  ApiClient(this.dio) {
+    dio = Dio(BaseOptions(
+      baseUrl: 'https://dummyjson.com',
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 10),
+      headers: {'Content-Type': 'application/json'},
+    ));
+
+    dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (options, handler) {
+        // Xử lý trước khi gửi request
+        print('Request [${options.method}] => PATH: ${options.path}');
+        EasyLoading.show();
+        return handler.next(options);
+      },
+      onResponse: (response, handler) {
+        // Xử lý khi nhận được response
+        EasyLoading.dismiss();
+        print('Response [${response.statusCode}] => DATA: ${response.data}');
+        return handler.next(response);
+      },
+      onError: (error, handler) {
+        // Xử lý khi có lỗi
+        EasyLoading.dismiss();
+        print('Error: ${error.message}');
+        return handler.next(error);
+      },
+    ));
+  }
+
+  Future<Response> post(
+    String path, {
+    Object? data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) async {
+    var result = await dio.post(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+      options: Options(
+        followRedirects: false,
+        validateStatus: (status) {
+          return (status ?? 0) < 1500;
+        },
+      ),
+    );
+
+    return result;
+  }
+
+  Future<Response> put(
+    String path, {
+    Object? data,
+  }) async {
+    var result = await dio.put(
+      path,
+      data: data,
+      options: Options(
+        followRedirects: false,
+        validateStatus: (status) {
+          return (status ?? 0) < 1500;
+        },
+      ),
+    );
+
+    return result;
+  }
+
+  Future<Response> get(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) async {
+    var result = await dio.get(
+      path,
+      queryParameters: queryParameters,
+      options: Options(
+        followRedirects: false,
+        validateStatus: (status) {
+          return (status ?? 0) < 1500;
+        },
+      ),
+    );
+
+    return result;
+  }
+
+  Future<Response> delete(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) async {
+    var result = await dio.delete(
+      path,
+      queryParameters: queryParameters,
+      options: Options(
+        followRedirects: false,
+        validateStatus: (status) {
+          return (status ?? 0) < 1500;
+        },
+      ),
+    );
+
+    return result;
+  }
 }
